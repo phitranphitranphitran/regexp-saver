46b5cee99b14a4e287d8637bb6242a3521b98d4d
Phi Tran <ptran@securitycompass.com>
2019-09-27 13:40:06 -0400

    implement basic replace

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,19 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "regexp-saver" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+		const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+		const selection: vscode.Selection = editor.selection;
+		if (selection.isEmpty) {
+			return;
+		}
+		const text: string = editor.document.getText(selection);
+		const regExp: RegExp = new RegExp('(\w+)=(.+)');
+		const newText: string = text.replace(regExp)
+		editor.edit(builder => builder.replace(selection, 'haha123'));
 	});
 
 	context.subscriptions.push(disposable);
@@ -25,3 +21,4 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
