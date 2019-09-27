9514fa2ac5bbb7fb6d771c2fa8d649a920074844
Phi Tran <ptran@securitycompass.com>
2019-09-27 16:38:01 -0400

    implement replaceInFile

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import { stringify } from 'querystring';
 
 export function activate(context: vscode.ExtensionContext) {
-	const commands = { replaceInSelection };
+	const commands = { replaceInSelection, replaceInFile };
 	for (const [name, fn] of Object.entries(commands)) {
 		const disposable = vscode.commands.registerCommand(`extension.${name}`, fn);
 		context.subscriptions.push(disposable);
@@ -18,8 +18,38 @@ async function replaceInSelection() {
 	if (selection.isEmpty) {
 		return;
 	}
+	const pickedItem = await pickSavedItem({ validateReplace: true });
+	if (!pickedItem) {
+		return;
+	}
+	const text = editor.document.getText(selection);
+	const regExp = new RegExp(pickedItem.regExp, pickedItem.flags || 'g');
+	const newText = text.replace(regExp, pickedItem.replacePattern);
+	editor.edit(builder => builder.replace(selection, newText));
+}
+
+async function replaceInFile() {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		return;
+	}
+	const pickedItem = await pickSavedItem({ validateReplace: true });
+	if (!pickedItem) {
+		return;
+	}
+	const text = editor.document.getText();
+	const regExp = new RegExp(pickedItem.regExp, pickedItem.flags || 'g');
+	const newText = text.replace(regExp, pickedItem.replacePattern);
+	const textRange = new vscode.Range(
+		editor.document.positionAt(0), 
+		editor.document.positionAt(text.length - 1)
+	);
+	editor.edit(builder => builder.replace(textRange, newText));
+}
+
+async function pickSavedItem({ validateReplace = false } = {}) {
 	const configuration = vscode.workspace.getConfiguration();
-	const savedItems: SavedItem[] | undefined = configuration.get('regExpSaver.saved');
+	const savedItems: any[] | undefined = configuration.get('regExpSaver.saved');
 	if (!savedItems || !savedItems.length) {
 		vscode.window.showErrorMessage('No RegExps were saved yet');
 		return;
@@ -35,19 +65,23 @@ async function replaceInSelection() {
 		vscode.window.showErrorMessage('Selected RegExp has no regExp specified');
 		return;
 	}
-	if (!pickedItem.replacePattern) {
+	if (validateReplace && !pickedItem.replacePattern) {
 		vscode.window.showErrorMessage('Selected RegExp has no replacePattern specified');
 		return;
 	}
-	const text = editor.document.getText(selection);
-	const regExp = new RegExp(pickedItem.regExp, pickedItem.flags || 'g');
-	const newText = text.replace(regExp, pickedItem.replacePattern);
-	editor.edit(builder => builder.replace(selection, newText));
+	return pickedItem;
 }
 
-interface SavedItem {
-	name?: string;
-	regExp?: string;
-	replacePattern?: string;
-	flags?: string;
-}
+// interface SavedItem {
+// 	name?: string;
+// 	regExp?: string;
+// 	replacePattern?: string;
+// 	flags?: string;
+// }
+
+// interface ValidatedSavedItem {
+// 	name: string;
+// 	regExp: string;
+// 	replacePattern?: string;
+// 	flags?: string;
+// }
