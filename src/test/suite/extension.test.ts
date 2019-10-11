5080676e24a4dcec07e37cbe43f492675ce5fcc3
Phi Tran <ptran@securitycompass.com>
2019-10-11 17:31:55 -0400

    set up tests, test replace in file

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,39 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
+import * as sinon from 'sinon';
+import { describe, it, before, beforeEach } from 'mocha';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+describe('Extension Test Suite', function() {
+	let document: vscode.TextDocument;
+	let textEditor: vscode.TextEditor;
 
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
+	before(async function() {
+		document = await vscode.workspace.openTextDocument();
+		textEditor = await vscode.window.showTextDocument(document);
+	});
+
+	beforeEach(async function() {
+		const currentText = textEditor.document.getText();
+		const documentTextRange = new vscode.Range(
+			textEditor.document.positionAt(0), 
+			textEditor.document.positionAt(currentText.length)
+		);
+		await textEditor.edit(builder => builder.delete(documentTextRange));
+	});
+
+	it('can replace in file', async function() {
+		const savedRegExp = {
+			label: 'Remove numbers',
+			regExp: '([a-z]+)[^a-z]*',
+			replacePattern: '$1'
+		};
+		sinon.stub(vscode.window, 'showQuickPick').callsFake(async () => savedRegExp);
+		const text = 'abc 123 def 456 !!!';
+		const expected = 'abcdef';
+		await textEditor.edit(builder => builder.replace(textEditor.document.positionAt(0), text));
+
+		await vscode.commands.executeCommand('regExpSaver.replaceInFile');
+
+		assert.equal(document.getText(), expected);
 	});
 });
