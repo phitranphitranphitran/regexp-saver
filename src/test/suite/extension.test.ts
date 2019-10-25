a1e5ed6200b975b27d07fbf9a723d212ad6797b4
Phi Tran <ptran@securitycompass.com>
2019-10-25 12:02:15 -0400

    expand test a bit

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,6 +1,7 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 import * as sinon from 'sinon';
+import dedent from 'ts-dedent';
 import { describe, it, before, beforeEach } from 'mocha';
 
 describe('Extension Test Suite', function() {
@@ -8,11 +9,13 @@ describe('Extension Test Suite', function() {
 	let textEditor: vscode.TextEditor;
 
 	before(async function() {
+		// Open a new text editor
 		document = await vscode.workspace.openTextDocument();
 		textEditor = await vscode.window.showTextDocument(document);
 	});
 
 	beforeEach(async function() {
+		// Delete all text in the document
 		const currentText = textEditor.document.getText();
 		const documentTextRange = new vscode.Range(
 			textEditor.document.positionAt(0), 
@@ -23,13 +26,21 @@ describe('Extension Test Suite', function() {
 
 	it('can replace in file', async function() {
 		const savedRegExp = {
-			label: 'Remove numbers',
-			regExp: '([a-z]+)[^a-z]*',
+			label: 'Replace line with abc if line contains abc',
+			regExp: '.*(abc).*',
 			replacePattern: '$1'
 		};
 		sinon.stub(vscode.window, 'showQuickPick').callsFake(async () => savedRegExp);
-		const text = 'abc 123 def 456 !!!';
-		const expected = 'abcdef';
+		const text = dedent`
+			abc 123 def 456 !!!
+			acbac blahblahabc?
+			dfbgidng*$%H@IGWhj
+			49th34abc ihg94y3`;
+		const expected = dedent`
+			abc
+			abc
+			dfbgidng*$%H@IGWhj
+			abc`;
 		await textEditor.edit(builder => builder.replace(textEditor.document.positionAt(0), text));
 
 		await vscode.commands.executeCommand('regExpSaver.replaceInFile');
